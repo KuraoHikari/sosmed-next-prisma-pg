d3e22abf0cea80ddeff99b9cd294658e3ff61b38
KuraoHikari <dewaindra705@gmail.com>
2023-05-12 13:24:38 +0800

    needs update

--- app/components/Heading/Heading.tsx
+++ app/components/Heading/Heading.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+'use client';
+
+import React from 'react';
 
 interface HeadingProps {
   title: string;
@@ -6,8 +8,13 @@ interface HeadingProps {
   center?: boolean;
 }
 
-const Heading: React.FC<HeadingProps> = () => {
-  return <div></div>;
+const Heading: React.FC<HeadingProps> = ({ title, subtitle, center }) => {
+  return (
+    <div className={center ? 'text-center' : 'text-start'}>
+      <div className="text-2xl font-bold">{title}</div>
+      <div className="font-light text-neutral-500 mt-2">{subtitle}</div>
+    </div>
+  );
 };
 
 export default Heading;
--- app/components/modals/auth/registerModal.tsx
+++ app/components/modals/auth/registerModal.tsx
@@ -1,17 +1,58 @@
-"use Client";
+'use client';
 
-import { Modal } from "../";
-import { Heading } from "../../Heading";
+import { useState } from 'react';
+import { Modal } from '../';
+import { Heading } from '../../Heading';
+import { Input } from '../../inputs';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 const RegisterModal = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+    },
+  });
   const bodyContent = (
     <div className="flex flex-col gap-4">
       <Heading title="Wellcome to KuraoSosmed" subtitle="Create an account!" />
+      <Input
+        id="email"
+        label="email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
+      <Input
+        id="password"
+        label="Password"
+        type="password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+      />
     </div>
   );
   return (
     <Modal
       isOpen={true}
+      body={bodyContent}
       //   onClose={() => {}}
       //   onSubmit={() => {}}
       actionLabel="Register"
